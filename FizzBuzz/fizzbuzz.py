5b3c14de822c20dbeb050de12e44a1817567b268
n0n0a <n0n0a6262@gmail.com>
2020-10-08 21:19:21 +0900

    feat: implement doctests

--- FizzBuzz/fizzbuzz.py
+++ FizzBuzz/fizzbuzz.py
@@ -1,4 +1,14 @@
 def fizzbuzz(n: int) -> str:
+    """Fizz Buzz function
+    >>> fizzbuzz(3)
+    'Fizz'
+    >>> fizzbuzz(5)
+    'Buzz'
+    >>> fizzbuzz(15)
+    'FizzBuzz'
+    >>> fizzbuzz(7)
+    '7'
+    """
     if n%15==0: return "FizzBuzz"
     if n%3==0: return "Fizz"
     if n%5==0: return "Buzz"

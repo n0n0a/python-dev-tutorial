d269d2633bc41879321285a31268ef2b03904dc8
Nao Goto <n0n0a6262@gmail.com>
2020-10-09 19:30:55 +0900

    feat: cli (#15)

--- FizzBuzz/fizzbuzz.py
+++ FizzBuzz/fizzbuzz.py
@@ -1,5 +1,6 @@
 import math
-
+import argparse
+import sys
 
 def fizzbuzz(n: int, a: int = 3, b: int = 5) -> str:
     """Fizz Buzz function
@@ -14,3 +15,27 @@ def fizzbuzz(n: int, a: int = 3, b: int = 5) -> str:
     if n % a == 0: return "Fizz"
     if n % b == 0: return "Buzz"
     return str(n)
+
+def main():
+    parser = argparse.ArgumentParser(description='Fizz Buzz program.')
+    parser.add_argument('nums', type=int, default=[], nargs='*',
+                        help="Number to be applied to FizzBuzz function. If no arguments passed, read from stdin.")
+    parser.add_argument('--fizz', type=int, default=3,
+                        help='Number corresponds to Fizz.')
+    parser.add_argument('--buzz', type=int, default=5,
+                        help='Number corresponds to Fizz.')
+    args = parser.parse_args()
+
+    if args.nums:  # 数字の列が引数から渡された場合には、それらの数字にFizzBuzzを適用する
+        sys.stderr.write("Reading numbers from arguments ...\n")
+        for n in args.nums:
+            sys.stdout.write(f"{fizzbuzz(n, fizz=args.fizz, buzz=args.buzz)}\n")
+    else:  # 数字の列が引数から渡されなかった場合には、標準入力から数字を読み込む
+        sys.stderr.write("Reading numbers from stdin ...\n")
+        try:
+            line = sys.stdin.readline()
+            while line:
+                sys.stdout.write(f"{fizzbuzz(int(line), fizz=args.fizz, buzz=args.buzz)}\n")
+                line = sys.stdin.readline()
+        except KeyboardInterrupt:
+            return

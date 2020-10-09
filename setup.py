eb6d248ad321a5a914087e00bc114f167e635f6a
n0n0a <n0n0a6262@gmail.com>
2020-10-09 20:32:13 +0900

    feat: click

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='fizzbuzz',
-    version="0.0.3",
+    version="0.0.4",
     description='Fizz Buzz program.',
     author='n0n0a',
     url='https://github.com/n0n0a/python-dev-tutorial',
     author_email='n0n0a6262@gmail.com',
     license='MIT',
-    install_requires=["pytest"],
+    install_requires=["pytest", "click"],
     packages=find_packages(exclude=["tests"]),
     entry_points={
         'console_scripts': 'fizzbuzz-cli = FizzBuzz.fizzbuzz:main'

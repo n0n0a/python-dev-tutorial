13ef8b163ab28d4af89acbcf1f086d8f1076b1d5
n0n0a <n0n0a6262@gmail.com>
2020-10-09 19:29:52 +0900

    feat: cli

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fizzbuzz',
-    version="0.0.1",
+    version="0.0.2",
     description='Fizz Buzz program.',
     author='n0n0a',
     url='https://github.com/n0n0a/python-dev-tutorial',
@@ -10,6 +10,9 @@ setup(
     license='MIT',
     install_requires=[],
     packages=find_packages(exclude=["tests"]),
+    entry_points={
+        'console_scripts': 'fizzbuzz-cli = FizzBuzz.fizzbuzz:main'
+    },
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License"
